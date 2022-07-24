781a415ab1aa34c4fceda44bf494cb02f296541a
Jitesh Bhashwani <jitubhashwani999@gmail.com>
2022-07-24 22:14:38 +0530

    menu updated

--- LinkedListAdt/main.cpp
+++ LinkedListAdt/main.cpp
@@ -1,13 +1,14 @@
 #include "linkedList.hpp"
 void menu()
 {
+	std::system("clear");
 	std::cout << "press 0: exit\n";
 	std::cout << "press 1: push()\n";
 	std::cout << "press 2: pop()\n";
 	std::cout << "press 3: isEmpty()\n";
 	std::cout << "press 4: isFull()\n";
 	std::cout << "press 5: display()\n";
-	std::cout << "press 6: clear screen[shell cmd]\n\n:";
+	std::cout << "press 6: clear screen[shell cmd]\n\n";
 	
 }
 int main() 
@@ -18,20 +19,30 @@ int main()
 	menu();
 	do
 	{
+		std::cout<<":";
 		std::cin >> choice;
 		switch (choice)
 		{
 		case 0: break;
 		case 1:
-			{int x; std::cin >> x;
-			// if(!q.isFUll()) std::cout << x << " added" << std::endl;
-			// q.push(x);
-			// node new_node(x);
-			// q.push(new_node);}
-			q.push(x);}
+		{
+			int x; std::cin >> x;
+			if(!q.isFUll())
+			{
+				std::cout << x << " added" << std::endl;
+			}
+			q.push(x);
+		}
 			break;
 		case 2:
-			std::cout << q.pop() << " dequed from queue" << std::endl;  
+		{
+			bool flag = !(q.isEmpty());
+			int dequed = q.pop();
+			if (flag)
+			{
+				std::cout << dequed << " dequed" << std::endl;  
+			}
+		}
 			break;
 		case 3:
 			std::cout << (q.isEmpty() ? "empty" : "not empty") << std::endl;
@@ -43,8 +54,8 @@ int main()
 			q.display();
 			break;
 		case 6:
-			std::system("clear");
 			menu();
+			break;
 		default:
 			break;
 		}
