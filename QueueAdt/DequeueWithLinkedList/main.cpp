08104ded1b67ab876f0215327a769a1a7648ed7c
Jitesh Bhashwani <jitubhashwani999@gmail.com>
2022-07-31 00:17:01 +0530

    dQueue init

--- QueueAdt/DequeueWithLinkedList/main.cpp
+++ QueueAdt/DequeueWithLinkedList/main.cpp
@@ -1,103 +1,58 @@
-#include <iostream>
-struct node
+#include "dQueue.hpp"
+
+void menu()
 {
-    int data;
-    node* previous;
-    node* next;
-    node(int data = 0) : data{data},previous{NULL},next{NULL}{}
-};
-class cQueue
-{
-private:
-    node *first;
-    node *last;
-    int _size;
-public:
-    cQueue():first{NULL},last{NULL},_size{0}{;}
-    ~cQueue();
-// Methods:
-    int front(){return this->first->data;}//
-    int back(){return this->last->data;}
-    bool empty(){return (first == NULL);}//
-    int dequeue();
-    void inqueue(int x);//
-    int size(){return this->_size;}//
-friend std::ostream& operator<<(std::ostream& os,const cQueue& that);
-};
-int cQueue::dequeue()
-{
-    int popped{-1};
-    if(this->empty())
-    {
-        std::cout << "stack undeflow" << std::endl;
-    }
-    else
-    {
-        popped = this->first->data;
-        node* second = this->first->next;
-        delete first;
-        first = second;
-        _size--;
-    }
-    return popped;
+	std::system("clear");
+	std::cout << "press 0: exit\n";
+	std::cout << "press 1: pop_back()\n";
+	std::cout << "press 2: pop_front()\n";
+	std::cout << "press 3: push_back()\n";
+	std::cout << "press 4: push_front()\n";
+	std::cout << "press 5: clear list\n";
 }
 
-cQueue::~cQueue()
-{
-    node* it = this->first;
-    while (this->first)
-    {
-        first = first->next;
-        delete it;
-        it = first;
-    }
-}
-void cQueue::inqueue(int x)
-{
-    if (this->empty())
-    {
-        this->first = this->last = new node(x);
-    }
-    else
-    {
-        this->last->next = new node(x);
-        this->last->next->previous = this->last;
-        this->last = this->last->next;
-    }
-    _size++;   
-}
-std::ostream& operator<<(std::ostream& os,const cQueue& that)
-{
-    node *temp = that.first;
-    os << "size(" << that._size << ") ";
-    while (temp)
-    {
-        os << temp->data << " ";
-        temp = temp->next;
-    }
-    os << std::endl;
-    return os;
-}
 int main()
 {
-    cQueue q;
-    q.inqueue(1);
-    q.inqueue(2);
-    q.inqueue(3);
-    q.inqueue(4);
-    q.inqueue(5);
-    std::cout << q << q.empty() << std::endl;
-    q.dequeue();
-    std::cout << q;
-    q.dequeue();
-    std::cout << q;
-    q.dequeue();
-    std::cout << q;
-    q.dequeue();
-    std::cout << q;
-    q.dequeue();
-    std::cout << q;
-    q.dequeue();
-    std::cout << q;
-    return 0;
+	dQueue q;
+
+	int choice;
+	do
+	{
+		menu();
+		std::cout << q << std::endl;
+		std::cout << ":";
+		std::cin >> choice;
+		switch (choice)
+		{
+		case 0:
+			break;
+		case 1:
+			q.pop_back();
+			break;
+		case 2:
+			q.pop_front();
+			break;
+		case 3:
+		{
+			int x;
+			std::cin >> x;
+			q.push_back(x);
+		}
+		break;
+		case 4:
+		{
+			int x;
+			std::cin >> x;
+			q.push_front(x);
+		}
+		break;
+		case 5:
+			q.clear();
+			break;
+		default:
+			break;
+		}
+	} while (choice != 0);
+
+	return 0;
 }
